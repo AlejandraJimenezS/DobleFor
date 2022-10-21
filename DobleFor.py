648ac765c9784f797ea05ae414f8203fd8f1ab13
AlejandraJimenezS <90272365+AlejandraJimenezS@users.noreply.github.com>
2022-10-21 12:55:05 -0500

    Add files via upload

---
+++ DobleFor.py
@@ -0,0 +1,13 @@
+"""
+author: Alejandra Jiménez Soto
+date: 10/21/2022
+
+"""
+
+suma=0
+for i in range(10):
+    print("i",i)
+    suma = suma +1
+print("suma ",suma)
+for c in "hola a todos":
+    print(c)
